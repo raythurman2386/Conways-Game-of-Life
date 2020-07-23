8d5e19eb9100ce1d28a30300ab78f4f693468b09
Raymond Thurman <raymondthurman5@gmail.com>
2020-07-22 23:30:04 -0400

    :construction: Added some comments on Game page

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -4,6 +4,15 @@ const Game = () => {
 	return (
 		<div>
 			<h1>Game Page</h1>
+			{/* Buttons, With the amount of buttons, they will need to be a component */}
+			{/* TODO: We will need a Start button */}
+			{/* TODO: We will need a Stop Button */}
+			{/* TODO: We will need a Clear Button */}
+			{/* TODO: We will need a Random Button */}
+			{/* TODO: We will need a slider for speed */}
+			{/* TODO: We will need a slider for grid size */}
+			{/* TODO: We will need a dropdown for presets */}
+			{/* Grid Component */}
 		</div>
 	);
 };
